a8a2cc1bbe1f2556075948804190dae872f3a291
HugeKC <20868603+HugeKC01@users.noreply.github.com>
2025-03-26 16:10:39 +0700

    fix

--- lib/posttest.dart
+++ lib/posttest.dart
@@ -72,43 +72,62 @@ class QuizPageState extends State<PostTestScreen> {
             child: Card(
               elevation: 5,
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      questions[questionIndex]['questionText'] as String,
-                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                  questions[questionIndex]['questionText'] as String,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Divider(),
+                SizedBox(height: 20.0),
+                ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+                  bool isSelected = questions[questionIndex]['selectedAnswer'] == answer;
+                  return Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(vertical: 5.0),
+                  child: ElevatedButton(
+                  onPressed: () => answerQuestion(answer),
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: isSelected ? Theme.of(context).colorScheme.secondary : null,
+                  foregroundColor: isSelected ? Colors.white : null,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(answer),
+                  ),
+                  ),
+                  );
+                }),
                     SizedBox(height: 20.0),
-                    ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-                      bool isSelected = questions[questionIndex]['selectedAnswer'] == answer;
-                      return ElevatedButton(
-                        onPressed: () => answerQuestion(answer),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: isSelected ? Theme.of(context).colorScheme.secondary : null,
-                          foregroundColor: isSelected? Colors.white : null,
-                        ),
-                        child: Text(answer),
-                      );
-                    }).toList(),
-                    SizedBox(height: 20.0),
+                    Divider(), // Add this line for the divider
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        ElevatedButton(
-                          onPressed: previousQuestion,
-                          child: Text('Previous'),
-                        ),
+                        if (questionIndex > 0)
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: previousQuestion,
+                              child: Text('Previous'),
+                            ),
+                          ),
                         if (questionIndex < questions.length - 1)
-                          ElevatedButton(
-                            onPressed: nextQuestion,
-                            child: Text('Next'),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: nextQuestion,
+                              child: Text('Next'),
+                            ),
                           )
                         else
-                          ElevatedButton(
-                            onPressed: submitQuiz,
-                            child: Text('Submit'),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: submitQuiz,
+                              child: Text('Submit'),
+                            ),
                           ),
                       ],
                     ),
