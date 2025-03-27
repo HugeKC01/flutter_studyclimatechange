3fd55c10a4b9c8f75c87bdb0dfbb74158c30cc62
ThanayosCMM <thanayos.srin@mail.kmutt.ac.th>
2025-03-27 11:19:21 +0700

    ทำหน้า module2 หัวเรื่องที่2.1 เป็นpage1 -- พร้อมใส่cardเป็นทางเข้าในm2_main ในโปรแกรมขณะนี้ยังไม่ได้ใส่รูป

--- lib/learningpage/module2/m2_main.dart
+++ lib/learningpage/module2/m2_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'm2_learn2_p1.dart'; // ตรวจสอบว่าได้ import ไฟล์นี้แล้ว
 
 class Module2Screen extends StatelessWidget {
   const Module2Screen({super.key});
@@ -6,9 +7,7 @@ class Module2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Module 2: Effects of Climate Change'),
-      ),
+      appBar: AppBar(title: Text('Module 2: Effects of Climate Change')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -18,7 +17,8 @@ class Module2Screen extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: Card(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0)),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
                   elevation: 5.0,
                   child: Column(
                     children: <Widget>[
@@ -28,7 +28,9 @@ class Module2Screen extends StatelessWidget {
                             width: double.infinity,
                             decoration: BoxDecoration(
                               color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(15.0),
+                              ),
                             ),
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
@@ -57,12 +59,71 @@ class Module2Screen extends StatelessWidget {
                 ),
               ),
             ),
+
+            // ยศ เพื่มcardหัวเรื่องที่2.1
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Card(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0)),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5.0,
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => m2_learn2_p1(),
+                              ), // นำทางไปยังหน้า m2_learn2_p1.dart
+                            );
+                          },
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primary,
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(15.0),
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Cover Section',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ListTile(
+                          title: Text('เรื่องที่2.1 อธิบายโลกร้อน'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Adapting and saving our world'),
+                              Text('การปรับตัวและช่วยโลกของเรา'),
+                            ],
+                          ),
+                          trailing: Icon(Icons.lock),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
                   elevation: 5.0,
                   child: Column(
                     children: <Widget>[
@@ -72,7 +133,9 @@ class Module2Screen extends StatelessWidget {
                             width: double.infinity,
                             decoration: BoxDecoration(
                               color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(15.0),
+                              ),
                             ),
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
