b6ddc560bd10c34b2e609ea82f6e7316705cb073
HugeKC <20868603+HugeKC01@users.noreply.github.com>
2025-04-05 15:05:57 +0700

    Implement reusable appbar in m3 files

--- lib/learningpage/module3/m3_main.dart
+++ lib/learningpage/module3/m3_main.dart
@@ -11,60 +11,7 @@ class Module3Screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Module 3: Adapting and saving our world'),
-        elevation: 5.0,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              child: IconButton(
-                icon: const Icon(Icons.person, color: Colors.white),
-                onPressed: () {
-                  // Handle the button press here
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home_rounded),
-              title: Text('Home'),
-              onTap: () {
-                // Handle the home tap here
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyHomePage(title: 'Climate Change App')),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.book_rounded),
-              title: Text('Manual'),
-              onTap: () {
-                // Handle the manual tap here
-              },
-            ),
-          ],
-        ),
+        title: Text('Module 1: Introduction to Climate Change'),
       ),
       body: Center(
         child: Column(
