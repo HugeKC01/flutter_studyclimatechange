57f385e930c243555a7f4c9982090a40e11487e8
Supawit.P <supawit.pool@kmutt.ac.th>
2025-04-16 23:52:26 +0700

    MINIgameHelpSomchaiFinish

--- lib/component/page_config.dart
+++ lib/component/page_config.dart
@@ -1,7 +1,7 @@
 class PageConfig {
   static const Map<String, int> lessonPageCounts = {
     'm1lesson1': 4,
-    'm1lesson2': 5,
+    'm1lesson2': 6,
     'm2lesson1': 3,
     'm2lesson2': 9,
   };
