ddb38a955ede9ad00bcaae6614efb916b1f08658
PatimaKruafu <patima.krua@kmutt.ac.th>
2025-05-16 12:10:33 +0700

    debug profile fix

--- lib/main.dart
+++ lib/main.dart
@@ -187,7 +187,7 @@ class _MyHomePageState extends State<MyHomePage>
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const ArticleScreen()),
+                MaterialPageRoute(builder: (context) => ArticleScreen()),
               );
             },
             child: const Icon(Icons.web),
